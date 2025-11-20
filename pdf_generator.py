56ef5ebf7889a24d3b37c1be2b4df150b2cba695
meldcrk <150430729+meldcrk@users.noreply.github.com>
2025-11-20 13:35:41 +0100

    Update pdf_generator.py

--- pdf_generator.py
+++ pdf_generator.py
@@ -6,6 +6,7 @@ Utilise reportlab pour créer des PDF professionnels
 from reportlab.lib.pagesizes import A4
 from reportlab.lib import colors
 from reportlab.lib.units import cm
+from reportlab.platypus import Image
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.enums import TA_CENTER, TA_LEFT
